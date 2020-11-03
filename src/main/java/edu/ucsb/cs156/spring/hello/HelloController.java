13bc464c97cdfbda8727df7386ce1535e991a0c5
Jiawei Yu <jiaweiyu2009@gmail.com>
2020-11-03 13:17:05 -0800

    done

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,10 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        String html = "<h1>Hello World!</h1>\n" +
+    "<p>This web app is powered by \n" +
+    "<a href='https://github.com/jiaweiyu2009/jpa02_cs156.git'>this github repo</a></p>\n";
+return html;
     }
     
 }
